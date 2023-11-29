d3b6038f480d5de7f96f9bce1be2f2d030e7235e
Jimena Hernández <her21199@uvg.edu.gt>
2023-11-29 13:02:37 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,11 +33,11 @@ function App() {
 // Componente FichaComponent con overlay
 const FichaComponent = ({ficha, links}) => (
   <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
-      <button onClick={() => setShowFicha(false)} className="absolute top-0 right-0 m-4 text-black text-3xl">&times;</button>
-      <div className="bg-white rounded-md custom-shadow w-full h-full flex flex-col justify-center items-center">
+      <button onClick={() => setShowFicha(false)} className="absolute top-4 right-2 md:top-10 md:right-10 m-4 text-black text-3xl">&times;</button>
+      <div className="bg-white rounded-md custom-shadow w-full h-full flex flex-col justify-start md:justify-center items-center">
           {
             ficha && (
-              <div className="flex flex-col justify-center items-center p-4 gap-2 md:p-8">
+              <div className="flex flex-col justify-center items-center p-4 gap-2 md:p-8 md:m-0 mt-12">
               <span className="text-black md:text-xl mt-2 w-full text-center font-bold">{ficha.name}</span>
               <h3 className="text-mcn-orange font-bold text-xs w-full md:text-lg">Descripción</h3>
               <span className="text-gray-500 text-xs md:text-lg mt-2 w-full">{renderTextWithLineBreaks(ficha.desc)}</span>
@@ -53,8 +53,22 @@ const FichaComponent = ({ficha, links}) => (
             )
           }
           {
-            links && (
+            ficha && links && (
               <div className="flex flex-col justify-center items-center p-4 gap-2">
+                <h3 className="text-mcn-orange font-bold text-xs w-full md:text-lg text-left">Recursos</h3>
+                {
+                  links.map((link, index) => (
+                    <a key={index} href={link} target="_blank" rel="noopener noreferrer" className="text-mcn-blue hover:text-blue-800 text-xs md:text-lg">
+                      {"Recurso " + (index + 1) }
+                    </a>
+                  ))
+                }
+              </div>
+            )
+          }
+          {
+            !ficha && links && (
+              <div className="flex flex-col justify-center items-center p-4 gap-2 md:m-0 mt-14">
                 <h3 className="text-mcn-orange font-bold text-xs w-full md:text-lg text-left">Recursos</h3>
                 {
                   links.map((link, index) => (
